4a1487d926e8c1897ee1c02790287bf1ddf0ebf8
dfsklar <dfsklar@gmail.com>
2017-11-07 23:57:30 -0500

    conv

---
+++ jsupdate.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# ONLY FOR UPDATING FORUM JS!!
+
+# does not update admin JS
+
+# This script compiles Flarum's core assets so that they can be used in-browser.
+# It should be run from the root directory of the core.
+
+/bin/rm -f ../../assets/forum*.js
+
+base=$PWD
+
+cd "${base}/js/forum"
+gulp
