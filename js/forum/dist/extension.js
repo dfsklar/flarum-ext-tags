2082b1f51d398cb3f4357421a05851be45cbfe19
dfsklar <dfsklar@gmail.com>
2017-11-19 22:35:11 -0500

    Display of forum leader

--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -907,6 +907,14 @@ System.register('flarum/tags/components/TagHero', ['flarum/Component'], function
 												var parent = app.store.getById('tags', tag.data.relationships.parent.data.id);
 												var leader = app.store.getById('users', parent.data.attributes.leaderUserId);
 
+												// If the leader's full info is not yet fetched from API, start that process and set up the
+												// promise to force a redraw of this component.
+												if (!leader) {
+														app.store.find('users', parent.data.attributes.leaderUserId).then(function () {
+																m.redraw();
+														});
+												}
+
 												return m(
 														'table',
 														{ 'class': 'marketing-block' },
@@ -927,12 +935,7 @@ System.register('flarum/tags/components/TagHero', ['flarum/Component'], function
 																				m(
 																						'div',
 																						{ 'class': 'group-leader-name' },
-																						parent.data.attributes.leaderUserId
-																				),
-																				m(
-																						'div',
-																						{ 'class': 'group-leader-name' },
-																						leader.data.attributes.displayName
+																						leader ? "This group's leader is: " + leader.data.attributes.displayName : ''
 																				),
 																				m(
 																						'div',
--- js/forum/src/components/TagHero.js
+++ js/forum/src/components/TagHero.js
@@ -5,15 +5,22 @@ export default class TagHero extends Component {
     const tag = this.props.tag;
 		const color = tag.color();
 		const parent = app.store.getById('tags', tag.data.relationships.parent.data.id);
-		const leader = app.store.getById('users', parent.data.attributes.leaderUserId);
+		var leader = app.store.getById('users', parent.data.attributes.leaderUserId);
 
-      return (
+		// If the leader's full info is not yet fetched from API, start that process and set up the
+		// promise to force a redraw of this component.
+		if (!leader) {
+			app.store.find('users', parent.data.attributes.leaderUserId).then(function(){
+				m.redraw();
+			})
+		}
+
+    return (
 	      <table class="marketing-block">
 	      <tbody><tr class="marketing-block">
 	      <td class="leftside">
 	      <div class="group-name">{parent.data.attributes.name}</div>
-	      <div class="group-leader-name">{parent.data.attributes.leaderUserId}</div>
-	      <div class="group-leader-name">{leader.data.attributes.displayName}</div>
+	      <div class="group-leader-name">{leader ? ("This group's leader is: " + leader.data.attributes.displayName) : ''}</div>
 	      <div class="group-summary">{parent.data.attributes.description}</div>
 				</td>
 	      <td class="rightside">
